a9cda3b8cf5f6a273632906188331062ab44dbb2
Shayan ShafieeMoghadam <shayan_shafieemoghadam@hamravesh.com>
2025-04-11 22:56:34 +0330

    2nd challenge

--- app/main.py
+++ app/main.py
@@ -2,16 +2,15 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    print(server_socket)
-    sock, _ = server_socket.accept() # wait for client
-    sock.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
-    sock.close()
+    conn, _ = server_socket.accept()
+    line = str(conn.recv(1024)).split("\\r\\n")[0]
+    [method, target, version] = line.split(" ")
+    if target == "/":
+        conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    else:
+        conn.sendall(b'HTTP/1.1 404 404 Not Found\r\n\r\n')
+    conn.close()
 
 
 if __name__ == "__main__":
